362ea94b8680d983659488b419192f8f0ad73494
Zack <56772875+zack-kyria@users.noreply.github.com>
2023-09-30 02:05:04 +0300

    Script to Update Pokemon GO
    You need to run this on the same directory your PoGo APK is located. Or you need to change the directory of the `apk_file` variable

---
+++ updatePoGo.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# ADB server kill and restart
+adb kill-server
+adb start-server
+
+# Specify the APK file name
+apk_file="YourApp.apk"
+
+# Read device list into array
+mapfile -t devices < "./DeviceList.txt"
+
+# Initialize device count
+deviceCount=0
+
+# Loop through each device and install APK then reboot
+for i in "${devices[@]}"; do
+    # Connect to the device
+    adb connect $i:5555
+
+    # Get device name
+    deviceName=$(adb -s $i:5555 shell getprop ro.product.model)
+    echo "Connecting to $i ($deviceName)"
+
+    # Install the APK
+    adb -s $i:5555 install -r $apk_file
+    echo "APK installed on $i ($deviceName)"
+
+    # Reboot the device
+    adb -s $i:5555 reboot
+    echo "Rebooted $i ($deviceName)"
+
+    # Increment device count
+    deviceCount=$((deviceCount + 1))
+done
+
+# Final summary and Discord notification
+echo "Installed APK and rebooted on $deviceCount devices."
