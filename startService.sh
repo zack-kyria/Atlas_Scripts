91071020633dd72d9fb7d6f95de842d3da66f357
Zack <56772875+zack-kyria@users.noreply.github.com>
2023-11-16 10:24:40 -0800

    Update startService.sh
    script to force start service when it is stopped

--- startService.sh
+++ startService.sh
@@ -3,10 +3,7 @@
 # Read device IPs from DeviceList.txt into an array
 mapfile -t devices < "./DeviceList.txt"
 
-# Define your module path
-module_path="eMagisk-Ethernet-Check-MOD.zip"
-
-# Loop over devices and install the module
+# Loop over devices and execute command
 for device in "${devices[@]}"; do
   adb connect $device:5555
   adb -s $device:5555 shell su -c "am startservice com.pokemod.atlas/com.pokemod.atlas.services.MappingService"
